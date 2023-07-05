6455f24fbf68d0fc8e46a77d562c9e84713fa719
rayhankinan <rayhankinan@gmail.com>
2023-07-05 20:00:12 +0700

    Add http method to panel

--- src/pages/api/panel.ts
+++ src/pages/api/panel.ts
@@ -3,7 +3,12 @@ import { renderTrpcPanel } from "trpc-panel";
 import { appRouter } from "~/server/api/root";
 import { env } from "~/env.cjs";
 
-export default function handler(_: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    res.status(405);
+    return;
+  }
+
   res.status(200).send(
     renderTrpcPanel(appRouter, {
       url: `${env.NEXT_PUBLIC_API_URL}/api/trpc`,
