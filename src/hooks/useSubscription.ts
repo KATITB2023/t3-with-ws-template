387e13c7d72001c5f436cb0427fca395f2967ad4
arsaizdihar <arsadihar@gmail.com>
2023-07-09 21:11:53 +0700

    pure useeffect without usecallback

--- src/hooks/useSubscription.ts
+++ src/hooks/useSubscription.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import type { ServerToClientEvents } from "~/server/socket/setup";
 import { socket } from "~/utils/socket";
 
@@ -9,17 +9,15 @@ function useSubscription<T extends keyof ServerToClientEvents>(
   cb: ServerToClientEvents[T],
   deps: any[] = []
 ) {
-  const _cb = useCallback(cb, deps);
-
   useEffect(() => {
     // @ts-expect-error typing is safe by the interface
-    socket.on(ev, _cb);
+    socket.on(ev, cb);
 
     return () => {
       // @ts-expect-error typing is safe by the interface
-      socket.off(ev, _cb);
+      socket.off(ev, cb);
     };
-  }, [_cb]);
+  }, deps);
 }
 
 export default useSubscription;
