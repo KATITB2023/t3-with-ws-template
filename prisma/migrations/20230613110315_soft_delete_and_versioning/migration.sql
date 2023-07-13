b88ea9e384885f8953c851b8ef951bb2ce1789c4
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-07-13 15:02:58 +0700

    integrate with shared database

--- prisma/migrations/20230613110315_soft_delete_and_versioning/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Example" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "version" INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "version" INTEGER NOT NULL DEFAULT 0;
--- prisma/migrations/20230615092127_generate_uuid_automatically/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Example" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
